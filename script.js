11a0e016f54621a021929f63deea1161df4f1437
sanyukta001 <sanyuktamishra911@gmail.com>
2023-03-26 12:29:51 +0530

    dictionary frontend using api

---
+++ script.js
@@ -0,0 +1,63 @@
+const input = document.querySelector('input')
+const btn = document.querySelector('button')
+
+const dictionaryArea = document.querySelector('.dictionary')
+
+async function dictionaryFn(word){
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(res => res.json())
+
+    // console.log(res)
+    return res[0]
+}
+
+btn.addEventListener('click', fetchandCreateCard)
+
+async function fetchandCreateCard(){
+    const data = await dictionaryFn(input.value)
+
+    console.log(data)
+
+    let partOfSpeechArray = []
+    for(let i = 0; i < data.meanings.length-1 ; i++)
+    {
+        partOfSpeechArray.push(data.meanings[i].partOfSpeechArray)
+    }
+
+    dictionaryArea.innerHTML = `
+    <div class="card">
+                <div class="property">
+                    <span>Word:</span>
+                    <span>${data.word}</span>
+                </div>
+
+                <div class="property">
+                    <span>Phonetics:</span>
+                    <span>${data.phonetic}</span>
+                </div>
+
+                <div class="property">
+                    <span>Audio:</span>
+                    <span>
+                    <audio controls src="${data.phonetics[0].audio}"></audio>
+                    </span>
+                </div>
+
+                <div class="property">
+                    <span>Definition:</span>
+                    <span>${data.meanings[0].definitions[0].definition}</span>
+                </div>
+
+                <div class="property">
+                    <span>Example:</span>
+                    <span>${data.meanings[1].definitions[0].example}</span>
+                </div>
+
+                <div class="property">
+                    <span>${partOfSpeechArray.map(e => e).join(', ')}</span>
+                </div>
+
+            </div>
+    `
+}
+
